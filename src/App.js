4ffe034ebd9b0ded12c7a635d8d785cffa650eea
gsouza <gsouza@luxfacta.com>
2020-06-01 17:40:37 -0300

    alteracao no layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useForm } from "react-hook-form"
+import { Button, Form } from 'react-bootstrap';
 
 function App() {
+ 
+  const {register, handleSubmit, error} = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
+    <div className="external-style">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Farmex</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Medicamento 1" name="medicamento01" ref={register}/>
+        <input type="text" placeholder="Medicamento 2" name="medicamento01" ref={register}/>
+        <input type="text" placeholder="Sintomas 01" name="sintoma01" ref={register}/>
+        <input type="text" placeholder="Sintomas 02" name="sintoma02" ref={register}/>
+        <input type="text" placeholder="Sintomas 03" name="sintoma03" ref={register}/>
+        {/* <Form.Control as="select" ref={register}>
+          <option>Dor de cabeça</option>
+          <option>Febre</option>
+          <option>Tosse</option>
+          <option>Tontura</option>
+          <option>Diabete</option>
+        </Form.Control> */}
+        <Button type="submit" variant="success">Enviar</Button>
+      </form>
+    </div>
     </div>
   );
 }
